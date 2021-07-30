0c7fc21d19de124b0f3de0cdc38ea0bb0b87d6df
tdy6284 <tdy6284@163.com>
2021-07-30 17:22:29 +0800

    'init'

---
+++ config.js
@@ -0,0 +1,7 @@
+window.__CONFIG__ = {
+  // provider: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+  // netid: 97,
+  provider: 'https://http-testnet.hecochain.com',
+  netid: 256,
+  debug: true
+}
